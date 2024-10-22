b196e857aefab2319bcedaa1e1cedf9036d8e70c
Eduardo Manzur Finotti Seabra <eduardo.manzur@dinfor.uniceub>
2024-10-22 15:55:03 -0300

    Funcionalidades

--- Essencial/src/componentes/Footer/index.jsx
+++ Essencial/src/componentes/Footer/index.jsx
@@ -6,14 +6,32 @@ import calcas from '../../assets/calças.png'
 import cinto from '../../assets/cinto.png'
 
 export default function Footer() {
+    const passar = () => {
+        let tudo = document.getElementById('itens');
+        let itens = Array.from(tudo.children);
+        if (itens.length > 0) {
+            let x = itens.shift();
+            tudo.appendChild(x);
+        }
+    }
+    const passar2 = () => {
+        let tudo = document.getElementById('itens');
+        let itens = Array.from(tudo.children);
+        if (itens.length > 0) {
+            let x = itens.pop();
+            tudo.insertBefore(x, tudo.firstChild); 
+        }
+    }
     return (
         <>
             <footer id="foot">
-                <img src={esquerda} alt="Seta para a esquerda" />
-                <img src={roupas} alt="Roupas Simples" className="img_foot" id="item1"/>
-                <img src={calcas} alt="Calças Jeans" className="img_foot"/>
-                <img src={cinto} alt="Cinto" className="img_foot"/>
-                <img src={direita} alt="Seta para a direita" />
+                <button onClick={passar} className="setas"><img src={esquerda} alt="Seta para a esquerda" id="esq" className="seta"/></button>
+                <div id="itens">
+                    <img src={roupas} alt="Roupas Simples" className="img_foot" id="item1"/>
+                    <img src={calcas} alt="Calças Jeans" className="img_foot" id="item2"/>
+                    <img src={cinto} alt="Cinto" className="img_foot" id="item3"/>
+                </div>
+                <button className="setas" onClick={passar2}><img src={direita} alt="Seta para a direita" id="dir" className="seta"/></button>
             </footer>
         </>
     )
--- Essencial/src/componentes/Product/index.jsx
+++ Essencial/src/componentes/Product/index.jsx
@@ -5,17 +5,31 @@ import Polo from '../../assets/polo.png'
 import Filtro from '../../assets/Filtro.png'
 
 export default function Product() {
+    const filtro = () => {
+
+    }
+
     return (
         <>
             <main id="prod">
                 <div id="filtro">
                     <div id="filt">
                         <button className="bot" id="esq">Populares</button>
-                        <button id="fil"><img src={Filtro} alt="Filtro" /></button>
+                        <button id="fil" onClick={filtro}><img src={Filtro} alt="Filtro" /></button>
                         <button className="bot" id="dir">Recentes</button>
                     </div>
-                    
                 </div>
+                <div id="parte">
+                    <div id="espaco"></div>
+                    <div id="filtragem">
+                        <div className="form">
+                            <label>Camisas</label>
+                            <input type="checkbox" />
+                        </div>
+                        <button id="botao-filtrar">Filtrar</button>
+                    </div>
+                </div>
+                
                 <div id="produtos">
                     <img src={Terno} alt="Terno" className="produto"/>
                     <img src={Colete} alt="Colete" className="produto"/>
